c0a74c90dc0112c26d7fa084789a16690b44b042
hkreds <109258421+hkreds@users.noreply.github.com>
2022-11-04 05:39:56 +0000

    [Add]グループ参加・退会機能追加

--- app/controllers/group_users_controller.rb
+++ app/controllers/group_users_controller.rb
@@ -1,4 +1,15 @@
 class GroupUsersController < ApplicationController
-  belongs_to :user
-  belongs_to :group
+  def create
+    @group = Group.find(params[:group_id])
+    group_user = current_user.group_users.new(group_id: @group.id)
+    group_user.save
+    redirect_to groups_path
+  end
+  
+  def destroy
+    @group = Group.find(params[:group_id])
+    group_user = current_user.group_users.find_by(group_id: @group.id)
+    group_user.destroy
+    redirect_to groups_path
+  end
 end
